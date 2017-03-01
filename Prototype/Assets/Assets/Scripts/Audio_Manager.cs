baaaaec6326336c88b21f94414b0cf5467b0d91f
Mark Key <markkey010@gmail.com>
2017-03-01 09:12:16 +0000

    Added Audio Mixer and volume controls - Mark.

---
+++ Prototype/Assets/Assets/Scripts/Audio_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Audio_Manager : MonoBehaviour {
+
+    public AudioMixer mainMixer;
+
+    public void SetMusicLvl(float musicLvl)
+    {
+        mainMixer.SetFloat("musicVol", musicLvl);
+    }
+    public void SetsfxLvl(float sfxLvl)
+    {
+        mainMixer.SetFloat("sfxVol", sfxLvl);
+    }
+}
